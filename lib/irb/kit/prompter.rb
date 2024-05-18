d732cebaa8c49db66298af1a4186b054bce4b8e4
Brooke Kuhlmann <brooke@alchemists.io>
2024-05-18 06:28:15 -0600

    Added prompter
    Necessary to display environment information within the prompt dynamically based on enviroment. At the moment, this only supports:
    
    * Ruby
    * Hanami
    * Ruby on Rails
    
    For testing purposes, both Hanami and Rails are faked to simplify testing and avoid having both frameworks loaded as test environment dependencies. This does mean if either of these frameworks change how they calculate application and environment details, these specs would not detect those changes. Opted to error on the side of simplicity for now.

---
+++ lib/irb/kit/prompter.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module IRB
+  module Kit
+    # Dynamically computes prompt based environment.
+    class Prompter
+      COLORS = {green: 32, red: 31}.freeze
+
+      def initialize version = RUBY_VERSION, delimiter: "|", colors: COLORS
+        super()
+        @version = version
+        @delimiter = delimiter
+        @colors = colors
+      end
+
+      def call
+        if defined? Hanami
+          details Hanami::VERSION, Hanami.app.name.delete_suffix("::App"), Hanami.env
+        elsif defined? Rails
+          details Rails.version, Rails.application.class.module_parent_name, Rails.env
+        else version_with_optional_project
+        end
+      end
+
+      private
+
+      attr_reader :version, :delimiter, :colors
+
+      def details framework_version, application_name, environment
+        [version, framework_version, application_name.downcase, color(environment)].join delimiter
+      end
+
+      def color environment
+        code = environment.to_sym == :production ? red : green
+        "\e[#{code}m#{environment}\e[0m"
+      end
+
+      def version_with_optional_project
+        File.basename(`git rev-parse --show-toplevel 2> /dev/null`.strip)
+            .downcase
+            .then { |project| [version, project].reject(&:empty?).join delimiter }
+      end
+
+      def green = colors.fetch __method__
+
+      def red = colors.fetch __method__
+    end
+  end
+end
---
+++ spec/lib/irb/kit/prompter_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe IRB::Kit::Prompter do
+  subject(:prompter) { described_class.new }
+
+  describe "#execute" do
+    let :hanami_fake do
+      Module.new do
+        def self.app = :"Hanami_Test::App"
+
+        def self.env = "development"
+      end
+    end
+
+    let :rails_fake do
+      Module.new do
+        def self.application
+          Module.new do
+            def self.class
+              Module.new { def self.module_parent_name = "rails_test" }
+            end
+          end
+        end
+
+        def self.env = "development"
+
+        def self.version = "1.2.3"
+      end
+    end
+
+    it "answers Ruby version and gem name" do
+      expect(prompter.call).to match(/\d+\.\d+\.\d+\|(irb-kit|project)/)
+    end
+
+    it "answers prompt with custom delimiter" do
+      prompter = described_class.new delimiter: "-"
+      expect(prompter.call).to match(/\d+\.\d+\.\d+-(irb-kit|project)/)
+    end
+
+    it "answers Ruby version, Hanami version, project name, and development environment" do
+      stub_const "Hanami", hanami_fake
+      stub_const "Hanami::VERSION", "1.1.1"
+
+      expect(prompter.call).to eq("#{RUBY_VERSION}|1.1.1|hanami_test|\e[32mdevelopment\e[0m")
+    end
+
+    it "answers Ruby version, Hanami version, project name, and production environment" do
+      allow(hanami_fake).to receive(:env).and_return "production"
+      stub_const "Hanami", hanami_fake
+      stub_const "Hanami::VERSION", "1.1.1"
+
+      expect(prompter.call).to eq("#{RUBY_VERSION}|1.1.1|hanami_test|\e[31mproduction\e[0m")
+    end
+
+    it "answers Ruby version, Rails version, project name, and development environment" do
+      stub_const "Rails", rails_fake
+      expect(prompter.call).to eq("#{RUBY_VERSION}|1.2.3|rails_test|\e[32mdevelopment\e[0m")
+    end
+
+    it "answers Ruby version, Rails version, project name, and production environment" do
+      allow(rails_fake).to receive(:env).and_return("production")
+      stub_const "Rails", rails_fake
+
+      expect(prompter.call).to eq("#{RUBY_VERSION}|1.2.3|rails_test|\e[31mproduction\e[0m")
+    end
+  end
+end
