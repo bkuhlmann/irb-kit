37517931601d4096f58c706467d2f90cdd4b661a
Brooke Kuhlmann <brooke@alchemists.io>
2024-05-18 06:28:15 -0600

    Added loader
    Necessary to load IRB extensions by namespace. The namespace is required to be specific to this gem.

---
+++ lib/irb/kit/loader.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module IRB
+  module Kit
+    # Loads IRB extensions for namespace.
+    class Loader
+      ALL = [:all].freeze
+
+      def initialize registrar, namespace, all: ALL
+        @registrar = registrar
+        @namespace = IRB::Kit.const_get namespace
+        @all = all
+      end
+
+      def call(*monikers) = monikers == all ? register_all : register_selected(*monikers)
+
+      private
+
+      attr_reader :registrar, :namespace, :all
+
+      def register_all = helpers.each { |helper| registrar.register helper.moniker, helper }
+
+      def register_selected(*monikers)
+        helpers.select { |helper| monikers.include? helper.moniker }
+               .then { |selected| monikers.zip selected }
+               .each { |moniker, helper| registrar.register moniker, helper }
+      end
+
+      def helpers = namespace.constants.sort.map { |name| namespace.const_get name }
+    end
+  end
+end
