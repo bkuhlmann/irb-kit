1681bd22adf4e796fe10fec636e68f5bdc79b857
Brooke Kuhlmann <brooke@alchemists.io>
2024-05-18 06:28:15 -0600

    Added search helper
    Necessary to search through all methods of an object by specific pattern.

---
+++ lib/irb/kit/helpers/search.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module IRB
+  module Kit
+    module Helpers
+      # Search an object's methods by pattern.
+      class Search < IRB::HelperMethod::Base
+        description "Search an object's methods by pattern."
+
+        def self.moniker = :search
+
+        def initialize regex = Regexp
+          super()
+          @regex = regex
+        end
+
+        def execute object, pattern
+          object.methods.grep regex.new(pattern)
+        rescue TypeError
+          "ERROR: Use only a string or regular expression for the pattern."
+        end
+
+        private
+
+        attr_reader :regex
+      end
+    end
+  end
+end
