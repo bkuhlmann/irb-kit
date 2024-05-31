348d71c12c6c34b2b71519bfee240ffbe75b8707
Brooke Kuhlmann <brooke@alchemists.io>
2024-05-30 20:03:44 -0600

    Fixed method and constant source code coverage
    Accidentally forgot to account for situations where there is no source to display.
    
    Milestone: patch

--- spec/lib/irb/kit/helpers/constant_source_spec.rb
+++ spec/lib/irb/kit/helpers/constant_source_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe IRB::Kit::Helpers::ConstantSource do
       expect(helper.execute("RUBY_DESCRIPTION")).to match(/ruby:0/)
     end
 
+    it "answers nil when there is no source" do
+      expect(helper.execute("HOME")).to be(nil)
+    end
+
     it "answers name error when constant is invalid" do
       expect(helper.execute(:foo)).to eq("ERROR: Invalid constant (:foo).")
     end
